cd0c2e5940ca6fb0607683e0da784a595dc446b1
MananJO09 <87749897+MananJO09@users.noreply.github.com>
2023-06-23 22:25:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 //your JS code here. If required.
-const time =document.getelementbyId("timer");
+const time =document.getElementById("timer");
 
 setInterval(() =>{
 	let d= new Date();
-	time.innerText=d.getSeconds();
+	// let hours = d.getHours().toString().padStart(2, "0");
+ //  let minutes = d.getMinutes().toString().padStart(2, "0");
+ //  let seconds = d.getSeconds().toString().padStart(2, "0");
+	// time.innerText= hours +":"+ minutes+":"+seconds+" "
+	time.innerText = d.toLocaleString();
 },1000)
